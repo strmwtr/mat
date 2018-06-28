0de3873dbc218d02713807516cdb877b65653655
Bob Brown <rb3@tutanota.com>
2018-06-28 10:45:06 -0400

    Working version, need to check test table before running on prod

--- mat.py
+++ mat.py
@@ -1,8 +1,14 @@
 import arcpy
 
-#MAT = r'Database Connections\Connection to GISPRDDB direct connect.sde\cvGIS.CITY.MASTERADDRESS'
-MAT = r'C:\Users\brownr\Desktop\db\clean\clean.gdb\mat'
+MAT = r'Database Connections\Connection to GISPRDDB direct connect.sde\cvGIS.CITY.MASTERADDRESS'
+#MATc = r'C:\Users\brownr\Desktop\db\clean\clean.gdb\mat_clean'
+#MAT = r'C:\Users\brownr\Desktop\db\clean\clean.gdb\mat'
 edit_fields = ['ST_NUMBER', 'ST_UNIT', 'UNIT_TYPE']
+
+def copy_mat():
+  arcpy.Delete_management(MAT)
+  arcpy.Copy_management(MATc, MAT)
+
 def get_set(field):
   with arcpy.da.SearchCursor(MAT, field) as cursor:
     values = set([row[0] for row in cursor])
@@ -21,25 +27,36 @@ def clean_unit():
     for row in cursor:
       if ' ' in row[0]:
         row[0] = row[0].replace(' ','')
-        print row[0]
         cursor.updateRow(row)
 
 def pound_to_unit():
-  unique = []
+  values = []
+  special = ['BACKFLOW', 'BSMT', 'COTT', 'DOWN', 'GARG', 'UP','UPPER']
   with arcpy.da.UpdateCursor(MAT, ['UNIT_TYPE','ST_UNIT']) as cursor:
     for row in cursor:
+      #List cases where UNIT_TYPE is # and ST_UNIT is not a num
       if row[0] == '#' and not row[1].isdigit():
-        unique.append(row[1])
-    for x in sorted(set(unique)):
-      print x
-      '''
-      if len(row[1]) == 1 and row[1].isalpha():
-        row[0] = 'UNIT'
-        cursor.updateRow(row)
-      '''
+        if row[1] in special:
+          row[0] = ''
+          cursor.updateRow(row)
+        else:
+          row[0] = 'UNIT'
+          cursor.updateRow(row)
+      if row[0] == '' and row[1] != '' and row[1] not in special:
+          row[0] = 'UNIT'
+          cursor.updateRow(row)
 
-pound_to_unit()
+def print_stuff():
+  special = ['BACKFLOW', 'BSMT', 'COTT', 'DOWN', 'GARG', 'UP','UPPER']
+  with arcpy.da.UpdateCursor(MAT, ['UNIT_TYPE','ST_UNIT']) as cursor:
+    for row in cursor:
+      # Changes # A to UNIT A
+      if row[0] == '' and row[1] != '' and row[1] not in special:
+        print row
 
 def calls():
   strip_all()
   clean_unit()
+  pound_to_unit()
+
+calls()
